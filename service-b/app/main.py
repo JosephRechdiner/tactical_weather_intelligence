ea4e73b5a3f9cd34f016f9271085cc862c02cb71
yoseftaktuk <taktuk07@gmail.com>
2026-01-20 15:08:26 +0200

    build Data_hendler class in utils and build add_temperature_categor_colume func in the class

--- service-b/app/main.py
+++ service-b/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 import routes
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(routes.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000, reload_delay=True)
+
