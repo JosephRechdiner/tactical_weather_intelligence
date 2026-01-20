8c02e56a7a809b6c17cb795ef7900e3a001bd9b6
JosephRechdiner <jrechdiner2002@gmail.com>
2026-01-20 17:04:26 +0200

    fixed little bug

--- service-c/main.py
+++ service-c/main.py
@@ -1,17 +1,5 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routes.records_route import records_router
-from db.connector import MySQLManager
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     MySQLManager.connect()
-#     yield
-#     MySQLManager.disconnect()
 
 app = FastAPI()
 app.include_router(records_router)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
