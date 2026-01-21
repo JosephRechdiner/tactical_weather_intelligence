cd76aceed324b49465107b3f8d494423ea9bd82f
yoseftaktuk <taktuk07@gmail.com>
2026-01-21 10:45:46 +0200

    fix post root so he can get date type

--- service-b/app/main.py
+++ service-b/app/main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 app.include_router(routes.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8000, reload_delay=True)
+    uvicorn.run(app, host='localhost', port=9060)
 
--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -2,14 +2,21 @@ from fastapi import FastAPI, HTTPException, APIRouter
 import models
 import utils
 
+
+
 app = FastAPI()
 router = APIRouter()
+send_service = utils.Send_data()
+
 
 @router.post('/ingest')
-def post_ingest(info: models.Weather_information):
+def post_ingest(info: list[models.Weather_information]):
     try:
-        utils.Data_hendler(info)
-        return {'massege': 'good'}
+        data = utils.Data_hendler(info)
+        data = data.return_data_dict()
+        send_service.send_data(data)
+        return {'massege': 'data send'}
     except HTTPException as e:
         return {'massege': str(e)}
+
 
