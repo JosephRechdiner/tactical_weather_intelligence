c32ea71a2c7ec2bf3066cc7092d2f3d9f40ef378
yoseftaktuk <taktuk07@gmail.com>
2026-01-21 17:27:26 +0200

    fix the valid error 2

--- service-c/main.py
+++ service-c/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from routes.records_route import records_router
-import uvicorn
+
 
 app = FastAPI()
 app.include_router(records_router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host='localhost', port=8080)
--- service-c/routes/records_route.py
+++ service-c/routes/records_route.py
@@ -4,51 +4,49 @@ from db.connector import MySQLManager
 from services.records_service import RecordsService
 from models.schemas import Info
 
-# manager = MySQLManager()
+manager = MySQLManager()
 records_router = APIRouter()
 
-@records_router.post("/records")
-def post_records(records: Info):
-    return {'msg': 'good'}
 
-# @records_router.post("/records")
-# def post_records(records: Info, cnx: MySQLConnection = Depends(manager.get_cnx)):
-#     try:      
-#         return RecordsService.insert_records(records.data, cnx)
-#     except Exception as e:
-#         raise HTTPException(status_code=404, detail=str(e))
+
+@records_router.post("/records")
+def post_records(records: Info, cnx: MySQLConnection = Depends(manager.get_cnx)):
+    try:      
+        return RecordsService.insert_records(records.data, cnx)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
     
-# @records_router.get("/all-records")
-# def get_all_records(cnx: MySQLConnection = Depends(manager.get_cnx)):
-#     try:
-#         return RecordsService.get_records(cnx)
-#     except Exception as e:
-#         raise HTTPException(status_code=404, detail=str(e))
+@records_router.get("/all-records")
+def get_all_records(cnx: MySQLConnection = Depends(manager.get_cnx)):
+    try:
+        return RecordsService.get_records(cnx)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
     
-# @records_router.get("/records/count")
-# def get_records_by_location(cnx: MySQLConnection = Depends(manager.get_cnx)):
-#     try:
-#         return RecordsService.get_number_of_records_by_location(cnx)
-#     except Exception as e:
-#         raise HTTPException(status_code=404, detail=str(e))
+@records_router.get("/records/count")
+def get_records_by_location(cnx: MySQLConnection = Depends(manager.get_cnx)):
+    try:
+        return RecordsService.get_number_of_records_by_location(cnx)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
     
-# @records_router.get("/records/avg-temperture")
-# def get_avg_temperture_by_location(cnx: MySQLConnection = Depends(manager.get_cnx)):
-#     try:
-#         return RecordsService.get_avg_temp_by_location(cnx)
-#     except Exception as e:
-#         raise HTTPException(status_code=404, detail=str(e))
+@records_router.get("/records/avg-temperture")
+def get_avg_temperture_by_location(cnx: MySQLConnection = Depends(manager.get_cnx)):
+    try:
+        return RecordsService.get_avg_temp_by_location(cnx)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
     
-# @records_router.get("/records/max-wind")
-# def get_max_wind(cnx: MySQLConnection = Depends(manager.get_cnx)):
-#     try:
-#         return RecordsService.get_max_wind_by_location(cnx)
-#     except Exception as e:
-#         raise HTTPException(status_code=404, detail=str(e))
+@records_router.get("/records/max-wind")
+def get_max_wind(cnx: MySQLConnection = Depends(manager.get_cnx)):
+    try:
+        return RecordsService.get_max_wind_by_location(cnx)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
     
-# @records_router.get("/records/extreme")
-# def get_extreme(cnx: MySQLConnection = Depends(manager.get_cnx)):
-#     try:
-#         return RecordsService.get_location_by_extreme(cnx)
-#     except Exception as e:
-#         raise HTTPException(status_code=404, detail=str(e))
+@records_router.get("/records/extreme")
+def get_extreme(cnx: MySQLConnection = Depends(manager.get_cnx)):
+    try:
+        return RecordsService.get_location_by_extreme(cnx)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
